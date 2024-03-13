a230169c0ccbde5085103ec034c61a36cfb66277
Aqib Nawaz <aqibnawazcs@gmail.com>
2024-03-13 12:52:45 +0500

    deployment and services

--- jokes-app-mern/jokes-api/app.js
+++ jokes-app-mern/jokes-api/app.js
@@ -11,36 +11,36 @@ main().catch(err => console.log(err));
 
 
 async function main() {
-   await mongoose.connect(`${process.env.MONGO_URI}`);  
+    await mongoose.connect(`${process.env.MONGO_URI}`);
 }
 
-mongoose.connection.on('connected',()=>{
+mongoose.connection.on('connected', () => {
     console.log("conneted to mongo yeahh")
 })
-mongoose.connection.on('error',(err)=>{
-    console.log("err connecting",err)
+mongoose.connection.on('error', (err) => {
+    console.log("err connecting", err)
 })
 
 app.use(express.json())
 app.use(cors())
 
-app.get('/getJokes',async (req,res)=>{
-       const jokes = await jokeModel.find({});
-       res.json({jokes})
+app.get('/getJokes', async (req, res) => {
+    const jokes = await jokeModel.find({});
+    res.json({ jokes })
 })
 
-app.post('/post-joke',async (req,res)=>{
-    try{
-        const newJoke =  await jokeModel.create({
-            joke:req.body.joke
+app.post('/post-joke', async (req, res) => {
+    try {
+        const newJoke = await jokeModel.create({
+            joke: req.body.joke
         })
-        res.json({newJoke:newJoke})
-    }catch(err){
+        res.json({ newJoke: newJoke })
+    } catch (err) {
         console.log(err)
     }
-   
+
 })
 
-app.listen(5000,()=>{
+app.listen(5000, () => {
     console.log("server running on 5000")
 })
